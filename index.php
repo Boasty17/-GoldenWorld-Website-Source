f0fbf94fbe30e132fe7ae2e3e693102d4ea5c6ae
Boasty17 <36826729+Boasty17@users.noreply.github.com>
2018-02-26 00:15:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<p> Bienvenue sur le site de GoldenWorld 2.0! Le serveur s'est fait hacker a cause d'un bot... Mais nous revenons bientot!
+<p> Bienvenue sur le site de GoldenWorld 2.0! Le serveur s'est fait hacker a cause d'un bot... Mais nous revenons bientot! <p>
